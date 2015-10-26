efabd18f931498275e6096eae4bff8e958393ddd
Jichao <jicahoo@163.com>
2015-10-26 20:33:17 +0800

    Change to SBT
    Change to SBT

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "AkkaInvestigation"
+
+version := "0.1"
+
+scalaVersion := "2.11.7"
+
+resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
+
+libraryDependencies ++= Seq (
+"com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT",
+"com.typesafe.akka" %% "akka-testkit" % "2.4-SNAPSHOT",
+"org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+)
---
+++ src/main/scala/zzz/akka/investigation/MainAkka.scala
@@ -0,0 +1,25 @@
+package zzz.akka.investigation
+
+import java.util.concurrent.Executors
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, ExecutionContext}
+
+/**
+ * Ran successfully on:
+ *    akka-actor_2.11-2.4.0
+ *    scala-sdk-2.11.7
+ */
+object MainAkka {
+  val pool = Executors.newCachedThreadPool()
+  implicit val ec = ExecutionContext.fromExecutorService(pool)
+
+  def main(args: Array[String]) {
+    val future = Future {
+      "Fibonacci Numbers"
+    }
+    val result = Await.result(future, 1.seconds)
+    println(result)
+    pool.shutdown()
+  }
+
+}
