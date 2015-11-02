2528dc3fdfa90cd65b2a5a9758e01bcd0f835a80
Jichao <jicahoo@163.com>
2015-11-02 23:11:44 +0800

    Refine unit test of FlightAttendant: set tick duration to 10 ms

--- src/test/scala/zzz/akka/avionics/TestFlightAttendant.scala
+++ src/test/scala/zzz/akka/avionics/TestFlightAttendant.scala
@@ -5,14 +5,18 @@ import akka.testkit.{TestKit, TestActorRef, ImplicitSender}
 import org.scalatest.WordSpecLike
 import org.scalatest.matchers.MustMatchers
 
+import com.typesafe.config.ConfigFactory
+
 object TestFlightAttendant {
   def apply() = new FlightAttendant with AttendantResponsiveness {
     val maxResponseTimeMS = 1
   }
 }
 
-class FlightAttendantSpec extends TestKit(ActorSystem("FlightAttendantSpec"))
+class FlightAttendantSpec
+  extends TestKit(ActorSystem("FlightAttendantSpec", ConfigFactory.parseString("akka.scheduler.tick-duration = 10ms")))
                           with ImplicitSender with WordSpecLike with MustMatchers {
+  //On windows, the min value of akka.scheduler.tick-duration is 10ms
   import FlightAttendant._
   "FlightAttendant" should {
     "get a drink when asked" in {
